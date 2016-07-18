7f23bca8b3ed30e49519f7d7457b686c74136bb7
HaniHashemi <jhanihashemi@gmail.com>
2016-07-18 14:54:17 +0430

    Update gradle version

--- sample/src/main/java/me/relex/circleindicator/sample/fragment/InfiniteViewPagerFragment.java
+++ sample/src/main/java/me/relex/circleindicator/sample/fragment/InfiniteViewPagerFragment.java
@@ -3,15 +3,13 @@ package me.relex.circleindicator.sample.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.antonyt.infiniteviewpager.InfinitePagerAdapter;
-
 import me.relex.circleindicator.CircleIndicator;
+import me.relex.circleindicator.InfinitePagerAdapter;
+import me.relex.circleindicator.InfiniteViewPager;
 import me.relex.circleindicator.sample.DemoPagerAdapter;
 import me.relex.circleindicator.sample.R;
 
@@ -21,20 +19,21 @@ import me.relex.circleindicator.sample.R;
  */
 public class InfiniteViewPagerFragment extends Fragment {
 
+    InfinitePagerAdapter wrappedAdapter;
+    InfiniteViewPager viewpager;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_default_demo, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        ViewPager viewpager = (ViewPager) view.findViewById(R.id.viewpager);
+        viewpager = (InfiniteViewPager) view.findViewById(R.id.viewpager);
         CircleIndicator indicator = (CircleIndicator) view.findViewById(R.id.indicator);
-        PagerAdapter wrappedAdapter = new InfinitePagerAdapter(new DemoPagerAdapter());
+        wrappedAdapter = new InfinitePagerAdapter(new DemoPagerAdapter());
         viewpager.setAdapter(wrappedAdapter);
         indicator.setViewPager(viewpager);
-        viewpager.setCurrentItem(2);
     }
 }
